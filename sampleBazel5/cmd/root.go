ce5fab675d45d73f1d9fd209f61b6393be042c6f
wangyu2 <wangyu2@sensetime.com>
2025-06-25 14:52:34 +0800

    feat:completed sampleBazel5

---
+++ sampleBazel5/cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/wrallen/sampleBazel5/config"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "root",
+	Short: "A Bazel + Cobra web service",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if _, err := config.Load(); err != nil {
+			panic(fmt.Sprintf("加载配置失败: %v", err))
+		}
+	},
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
---
+++ sampleBazel5/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/wrallen/sampleBazel5/cmd"
+
+func main() {
+	cmd.Execute()
+}
