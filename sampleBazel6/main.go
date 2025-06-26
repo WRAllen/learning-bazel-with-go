bc7af7005b150106c3035334c9ac53d438564169
wangyu2 <wangyu2@sensetime.com>
2025-06-26 10:10:54 +0800

    feat:add sampleBazel6 base

---
+++ sampleBazel6/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/wrallen/sampleBazel6/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ sampleBazel6/pkg/service.go
@@ -0,0 +1,40 @@
+package pkg
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/wrallen/sampleBazel6/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func helloHandler(logger *zap.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		logger.Info("Received request", zap.String("method", r.Method), zap.String("url", r.URL.String()))
+		fmt.Fprintln(w, "Hello, World! 🌍")
+	}
+}
+
+func Run() {
+	cfg := zap.NewProductionConfig()
+	// 自定义时间格式
+	cfg.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05.000")
+	// 改成更常见的字段名
+	cfg.EncoderConfig.TimeKey = "time"
+	logger, err := cfg.Build()
+	if err != nil {
+		panic(fmt.Sprintf("Cannot initialize zap logger: %v", err))
+	}
+	defer logger.Sync()
+
+	port := config.Global.Port
+	logger.Info("Starting server", zap.String("port", port))
+
+	http.HandleFunc("/", helloHandler(logger))
+
+	err = http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		logger.Fatal("Server failed to start", zap.Error(err))
+	}
+}
